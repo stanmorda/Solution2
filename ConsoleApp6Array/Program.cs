9d0be4e4b1071c294d8f470eced8b84857d8cc6c
Stanislav Mordanov <su.mordanov@stc.team>
2023-04-25 19:54:51 +0300

    - dictionary

--- ConsoleApp6Array/Program.cs
+++ ConsoleApp6Array/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Products;
 
 namespace ConsoleApp6Array
 {
@@ -7,6 +9,98 @@ namespace ConsoleApp6Array
     {
         static void Main(string[] args)
         {
+
+            // void addOrUpdate(Dictionary<int, string> dictionary, int key, string value)
+            // {
+            //     // dictionary[key] = value;
+            //     
+            //     if (dictionary.ContainsKey(key))
+            //     {
+            //         dictionary[key] = value;
+            //     }
+            //     else
+            //     {
+            //         dictionary.Add(key, value);
+            //     }
+            // }
+
+            var milk1 = new Product(100, "Milk");
+            
+            var milk2 = new Product(100, "Milk");
+            
+            var pr1 = new Product(80, "1");
+            var pr2 = new Product(90, "2");
+            
+            var pr3 = new Product(70, "3");
+            var pr4 = new Product(70, "3");
+            
+            Console.WriteLine($"{milk2 == milk2}");
+            
+            var list = new List<Product>();
+            //
+            list.Add(milk1);
+            list.Add(milk2);
+            
+            
+            //
+            // list.RemoveAll(x => x == milk);
+            //
+            
+            //Console.WriteLine($"{milk == milk1}");
+
+            Dictionary<Product, int> products = new Dictionary<Product, int>();
+            products.Add(milk1, 100);
+            products.Add(milk2, 0);
+            
+            // products.Add(milk, 99);
+            // if(products.Remove(milk1))
+            //     Console.WriteLine("REMOVED");
+            //
+            // Console.WriteLine($"{milk1.GetHashCode()}");
+
+            return;
+            
+            Dictionary<string, string> books = new Dictionary<string, string>();
+
+            books["hsgdhghs"] = "sdfsdf";
+            
+            string value;
+            
+            
+            
+            // if (books.ContainsKey(1))
+            // {
+            //     value = books[1];
+            // }
+            //
+            // books.TryGetValue(1, out value);
+
+            //books[100] = "fsdf";
+
+            // if(books.TryAdd(100, "sdf"))
+            //     Console.WriteLine("ADDED");
+
+            // books.Add(1, "aaa");
+            // books.Add(999, "bbb");
+            // books.Add(-1055, "ccc");
+            //
+            // if(!books.ContainsKey(-1055))
+            //     books.Add(-1055, "balbadflbadlfb");
+            //
+            // Console.WriteLine(books.Remove(77));
+
+            return;
+            // int[] array = new int[1000000000];
+            //
+            // var sw = Stopwatch.StartNew();
+            // for (int i = 0; i < array.Length; i++)
+            // {
+            //     int item = array[i];
+            // }
+            // sw.Stop();
+            //
+            // Console.WriteLine(sw.Elapsed.Milliseconds);
+            
             // Stack<int> stack = new Stack<int>();
             //
             // for (int i = 0; i < 10; i++)
