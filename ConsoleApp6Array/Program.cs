8574d1e05ab3e57d1632c37341711ee9602fcef8
Stanislav Mordanov <su.mordanov@stc.team>
2023-04-28 21:01:32 +0300

    - commit

--- ConsoleApp6Array/Program.cs
+++ ConsoleApp6Array/Program.cs
@@ -40,24 +40,58 @@ namespace ConsoleApp6Array
 
             var milk1 = new Product(100, "Milk");
             var milk2 = new Product(100, "Milk");
+            
+            var milk3 = new Product(102, "Milk");
             var pr1 = new Product(80, "1"){IsNew = true};
             var pr2 = new Product(80, "2");
+            var pr22 = new Product(60, "22");
+            var pr23 = new Product(20, "23");
+            var pr24 = new Product(80, "24");
             var pr3 = new Product(70, "3"){IsNew = true};
             var pr4 = new Product(70, "4");
             var pr5 = new Product();
             pr5.Id = 123;
 
-            var card1 = new ProductCard(new List<Product>() { milk1, pr2, pr1 });
-            var card2 = new ProductCard(new List<Product>() { milk2, pr3, pr4 });
+            var card1 = new ProductCard(new List<Product>() { milk1, });
+            var card2 = new ProductCard(new List<Product>() { milk1 });
+
+            var linq = card1.Items.Intersect(card2.Items).ToArray();
+            
             var card3 = new ProductCard(new List<Product>() { milk2, pr3, pr4,milk1, pr1, pr2 });
 
             var listOfCard = new List<ProductCard>() { card1, card2, card3 };
 
-            var newProd = listOfCard
+            
+            
+            // Except: возвращает разность двух коллекций, то есть те элементы, которые создаются только в одной коллекции
+            // Union: объединяет две однородные коллекции
+            // Intersect: возвращает пересечение двух коллекций, то есть те элементы, которые встречаются в обоих коллекциях
+            // Concat: объединяет две коллекции
+                
+                
+            var listOfUniqeTitles = listOfCard
                 .SelectMany(x => x.Items)
-                .Where(x => x.IsNew == true)
+                .Select(x => x.Title)
+                .Distinct()
+                .OrderBy(x => x)
                 .ToArray();
 
+            // var first = listOfUniqeTitles..First();
+            // var last = listOfUniqeTitles.Last();
+            
+            var groups = listOfCard
+                .SelectMany(x => x.Items)
+                .GroupBy(x=>x.Title)
+                .Select(x => new { Title = x.Key, Count = x.Count()})
+                .Where(x=>x.Count>3)
+                .ToArray();
+
+            var groups1 = listOfCard
+                .SelectMany(x => x.Items)
+                .GroupBy(x => x.IsNew)
+                .Select(x => new { Title = x.Key ? "NEW" : "OLD", Sum = x.Sum(y => y.Price) })
+                .ToArray();
+            
 
             var contains = card1.Items.Contains(milk1);
 
@@ -76,19 +110,18 @@ namespace ConsoleApp6Array
                 .IsNull()
                 .Select(x => x.Title)
                 .ToArray();
-                
 
             
 
             //var2
             var allProdsByLinq = listOfCard.SelectMany(x => x.Items).ToList();
             
-            
-            
-            var items = card1.Items
-                .OrderBy(x => x.Price)
-                .ThenBy(x=>x.Title)
-                .ToArray();
+            //
+            //
+            // var items = card1.Items.
+            //     .OrderBy(x => x.Price)
+            //     .ThenBy(x=>x.Title)
+            //     .ToArray();
 
 
             // Reverse: располагает элементы в обратном порядке
@@ -105,9 +138,9 @@ namespace ConsoleApp6Array
             // ThenBy: задает дополнительные критерии для упорядочивания элементов возрастанию
             // ThenByDescending: задает дополнительные критерии для упорядочивания элементов по убыванию
 
-            var sum = newProd.Sum(x => x.Price);
-            var count = newProd.Count();
-            
+            // var sum = newProd.Sum(x => x.Price);
+            // var count = newProd.Count();
+            //
             // 1. count
             // 2. sum
 
