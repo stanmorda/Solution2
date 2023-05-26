c30efc978b7c750b4ee1a31b673241f9268c35ba
Stanislav Mordanov <su.mordanov@stc.team>
2023-05-26 20:57:57 +0300

    - Task.WaitAll

--- TestTasks/Program.cs
+++ TestTasks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,11 +11,60 @@ namespace TestTasks
 
         static void Main(string[] args)
         {
+            // 1st var
+            // var task = new Task(PrintTimeAction);
+            // task.Start();
+            
+            // 2nd var
+            //var task = Task.Factory.StartNew(PrintTimeAction); // task1
+            //Task.Factory.StartNew(PrintRandomNumber); // task2
+            
+            
+            // 3d var
+            //var task = Task.Run(PrintRandomNumber);
+            
+            // 4th var
+            //Parallel.Invoke(PrintRandomNumber, PrintTimeAction);
 
+
+            // var task = new Task<int>(Calc);
+            // task.Start();
+            // Console.WriteLine($"task result is {task.Result}");
 
             
-            var task = new Task(PrintTimeAction);
-            task.Start();
+
+            // Parallel.Invoke(LongWork);
+            
+            // Task.Factory.StartNew(LongWork);
+
+            // var time = Stopwatch.StartNew();
+            //
+            // var r1 = Task.Factory.StartNew(LongWork1).Result;
+            // var r2 = Task.Factory.StartNew(LongWork2).Result;
+            //
+            // time.Stop();
+            //
+            // Console.WriteLine($"RESULT is {r1+r2}. TIME is {time.ElapsedMilliseconds}ms");
+            
+            var time = Stopwatch.StartNew();
+            var task1 = Task.Factory.StartNew(LongWork1);
+            var task2 = Task.Factory.StartNew(LongWork2);
+            
+            Task.WaitAll(task1, task2);
+            
+            time.Stop();
+            
+            Console.WriteLine($"RESULT is {task1.Result + task2.Result}. TIME is {time.ElapsedMilliseconds}ms");
+            
+            
+            // while (true)
+            // {
+            //     Console.WriteLine("main thread");
+            //     Thread.Sleep(500);
+            // }
+            
+            
+            return;
             
             
 
@@ -37,6 +87,20 @@ namespace TestTasks
             Console.WriteLine("Hello World!");
         }
 
+
+        private static void PrintRandomNumber()
+        {
+            while (true)
+            {
+                var random = new Random();
+                lock (_sync)
+                {
+                    Console.WriteLine($"Random number is {random.Next(10)}");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        
         private static void PrintTimeAction()
         {
             while (true)
@@ -52,6 +116,23 @@ namespace TestTasks
         private static int CalculateSum(int a, int b)
         {
             return a + b;
+        }
+
+        private static int Calc()
+        {
+            return 5 + 4;
+        }
+
+        private static int LongWork1()
+        {
+            Thread.Sleep(2000);
+            return Calc();
+        }
+        
+        private static int LongWork2()
+        {
+            Thread.Sleep(5000);
+            return Calc() * Calc()* Calc();
         }
     }
 }
